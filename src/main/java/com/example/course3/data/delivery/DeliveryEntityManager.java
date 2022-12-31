87dae37d3036e811cca58d0ded4c54d652076db9
Felix <busayofelix@gmail.com>
2022-12-31 05:47:59 +0100

    Adding persistence to Delivery Objects and also specify how associate objects like "plant" should load with it.

---
+++ src/main/java/com/example/course3/data/delivery/DeliveryEntityManager.java
@@ -0,0 +1,13 @@
+package com.example.course3.data.delivery;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+public class DeliveryEntityManager {
+
+    @PersistenceContext
+    EntityManager entityManager;
+
+
+
+}
